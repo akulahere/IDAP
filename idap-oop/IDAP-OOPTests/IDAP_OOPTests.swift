8f9fe33c976bc0f2a7cff9061aa5b6dda065bcc1
Dmytro Akulinin <akulinind@gmail.com>
2023-05-04 12:54:43 +0300

    implement first quick test

--- idap-oop/IDAP-OOPTests/IDAP_OOPTests.swift
+++ idap-oop/IDAP-OOPTests/IDAP_OOPTests.swift
@@ -6,31 +6,34 @@
 //
 
 import XCTest
+import Quick
+import Nimble
 @testable import IDAP_OOP
 
-final class IDAP_OOPTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+final class ChildTests: QuickSpec {
+    override func spec() {
+        describe("Child") {
+            var child: Child!
+            var father: Human!
+            var mother: Human!
+            
+            beforeEach {
+                father = Human(name: "John", age: 35, gender: .male)
+                mother = Human(name: "Jane", age: 32, gender: .female)
+                child = Child(name: "Alice", age: 5, gender: .female, parents: (father, mother))
+            }
+            
+            afterEach {
+                child = nil
+                father = nil
+                mother = nil
+            }
+            
+            it("should have the correct initial state") {
+                expect(child.name).to(equal("Alice"))
+                expect(child.gender).to(equal(Gender.female))
+            }
         }
     }
-
 }
+
