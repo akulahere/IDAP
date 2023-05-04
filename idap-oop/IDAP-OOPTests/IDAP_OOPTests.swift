c5c3d26fdd7373888a5866a1880be5af27063b08
Dmytro Akulinin <akulinind@gmail.com>
2023-05-04 15:38:23 +0300

    switch back to force unwrap according to the Quick manual

--- idap-oop/IDAP-OOPTests/IDAP_OOPTests.swift
+++ idap-oop/IDAP-OOPTests/IDAP_OOPTests.swift
@@ -12,7 +12,7 @@ import Nimble
 final class HumanSpec: QuickSpec {
     override func spec() {
         describe("Human") {
-            var human: Human?
+            var human: Human!
             
             beforeEach {
                 human = Human(name: "John", age: 30, gender: .male)
@@ -21,23 +21,23 @@ final class HumanSpec: QuickSpec {
             // MARK: Human init
             
             it("should have correct initial values") {
-                expect(human?.name).to(equal("John"))
-                expect(human?.age).to(equal(30))
-                expect(human?.gender).to(equal(Gender.male))
+                expect(human.name).to(equal("John"))
+                expect(human.age).to(equal(30))
+                expect(human.gender).to(equal(Gender.male))
             }
             
             // MARK: Age increasing
             
             it("should increase age by one when calling increaseAge") {
-                human?.increaseAge()
-                expect(human?.age).to(equal(31))
+                human.increaseAge()
+                expect(human.age).to(equal(31))
             }
         }
         
         describe("Child") {
-            var child: Child?
-            var father: Human?
-            var mother: Human?
+            var child: Child!
+            var father: Human!
+            var mother: Human!
             
             beforeEach {
                 father = Human(name: "John", age: 30, gender: .male)
@@ -48,18 +48,18 @@ final class HumanSpec: QuickSpec {
             // MARK: Child init
             
             it("should have correct initial values") {
-                expect(child?.name).to(equal("Billy"))
-                expect(child?.age).to(equal(0))
-                expect(child?.gender).to(equal(Gender.male))
-                expect(child?.parents.father).to(beIdenticalTo(father))
-                expect(child?.parents.mother).to(beIdenticalTo(mother))
+                expect(child.name).to(equal("Billy"))
+                expect(child.age).to(equal(0))
+                expect(child.gender).to(equal(Gender.male))
+                expect(child.parents.father).to(beIdenticalTo(father))
+                expect(child.parents.mother).to(beIdenticalTo(mother))
             }
         }
         
         describe("Person") {
-            var person: Person?
-            var father: Human?
-            var mother: Human?
+            var person: Person!
+            var father: Human!
+            var mother: Human!
             
             beforeEach {
                 father = Human(name: "John", age: 30, gender: .male)
@@ -70,14 +70,14 @@ final class HumanSpec: QuickSpec {
             // MARK: Correct Person Init
             
             it("should have correct initial values") {
-                expect(person?.name).to(equal("Alice"))
-                expect(person?.age).to(equal(25))
-                expect(person?.gender).to(equal(Gender.female))
-                expect(person?.isMarried).to(beFalse())
-                expect(person?.partner).to(beNil())
-                expect(person?.parents.father).to(beIdenticalTo(father))
-                expect(person?.parents.mother).to(beIdenticalTo(mother))
-                expect(person?.children).to(beEmpty())
+                expect(person.name).to(equal("Alice"))
+                expect(person.age).to(equal(25))
+                expect(person.gender).to(equal(Gender.female))
+                expect(person.isMarried).to(beFalse())
+                expect(person.partner).to(beNil())
+                expect(person.parents.father).to(beIdenticalTo(father))
+                expect(person.parents.mother).to(beIdenticalTo(mother))
+                expect(person.children).to(beEmpty())
             }
             
             context("after marriege") {
@@ -85,14 +85,14 @@ final class HumanSpec: QuickSpec {
                 
                 beforeEach {
                     partner = Person(name: "Bob", age: 27, gender: .male, isMarried: false, partner: nil, parents: (nil, nil))
-                    person?.becomeMarried(partner: partner)
+                    person.becomeMarried(partner: partner)
                 }
                 
                 // MARK: Marriage
                 
                 it("should update isMarried and partner properties") {
-                    expect(person?.isMarried).to(beTrue())
-                    expect(person?.partner).to(beIdenticalTo(partner))
+                    expect(person.isMarried).to(beTrue())
+                    expect(person.partner).to(beIdenticalTo(partner))
                     expect(partner.isMarried).to(beTrue())
                     expect(partner.partner).to(beIdenticalTo(person))
                 }
@@ -101,13 +101,13 @@ final class HumanSpec: QuickSpec {
                 
                 it("should be able to make a child") {
                     person?.makeChild(name: "Cathy", gender: .female, with: partner)
-                    expect(person?.children).to(haveCount(1))
+                    expect(person.children).to(haveCount(1))
                     expect(partner.children).to(haveCount(1))
-                    expect(person?.children.first?.name).to(equal("Cathy"))
-                    expect(person?.children.first?.gender).to(equal(Gender.female))
+                    expect(person.children.first?.name).to(equal("Cathy"))
+                    expect(person.children.first?.gender).to(equal(Gender.female))
                     expect(partner.children.first?.gender).to(equal(Gender.female))
-                    expect((person?.children.first as? Child)?.parents.father).to(beIdenticalTo(person?.partner))
-                    expect((person?.children.first as? Child)?.parents.mother).to(beIdenticalTo(person))
+                    expect((person.children.first as? Child)?.parents.father).to(beIdenticalTo(person.partner))
+                    expect((person.children.first as? Child)?.parents.mother).to(beIdenticalTo(person))
                 }
                 
                 context("when divorced") {
@@ -118,8 +118,8 @@ final class HumanSpec: QuickSpec {
                     // MARK: Divorcing
                     
                     it("should update isMarried and partner properties") {
-                        expect(person?.isMarried).to(beFalse())
-                        expect(person?.partner).to(beNil())
+                        expect(person.isMarried).to(beFalse())
+                        expect(person.partner).to(beNil())
                         expect(partner.isMarried).to(beFalse())
                         expect(partner.partner).to(beNil())
                     }
