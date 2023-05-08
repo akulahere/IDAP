43cbe8570b908e8c22a65d7f461f8b2e69d57e4b
Dmytro Akulinin <akulinind@gmail.com>
2023-05-08 14:49:16 +0300

    add observer project

---
+++ IDAP-observer/IDAP-observer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IDAP-observer
+//
+//  Created by Dmytro Akulinin on 08.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
