301174d3603e2353e233338982ca900db79e0484
Dmytro Akulinin <akulinind@gmail.com>
2023-05-23 11:34:07 +0300

    set up base xib project

---
+++ IDAP-Square/IDAP-Square/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  IDAP-Square
+//
+//  Created by Dmytro Akulinin on 23.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var square: UIView?
+    
+    override func viewDidLoad() {
+        print("did load")
+//        view.backgroundColor = .red
+        super.viewDidLoad()
+    }
+
+
+}
+
